d10d693ace1501fdf632ed76fb9ab24b25cc9163
Wyatt Anderson <wyatt.anderson@level.com>
2023-02-23 13:51:24 -0500

    make sure wkt works

--- visionv2/main.go
+++ visionv2/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	_ "cloud.google.com/go/vision/v2/apiv1"
 	_ "cloud.google.com/go/vision/v2/apiv1/visionpb"
 	_ "google.golang.org/grpc"
 
-	_ "github.com/wyattanderson/googleapis-reproducer/proto"
+	"github.com/wyattanderson/googleapis-reproducer/proto"
+	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 func main() {
 	fmt.Println("hello from v2")
+
+	_ = &proto.GoogleCloudVision{
+		Duration: durationpb.New(1 * time.Second),
+	}
 }
